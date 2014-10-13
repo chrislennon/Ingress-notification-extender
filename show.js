050e61563f693fa14881efd75c1754b126bcae10
Dion Segijn <dionsegijn@hotmail.com>
2014-10-14 00:11:38 +0200

    add file for showing notification

---
+++ show.js
@@ -0,0 +1,11 @@
+chrome.notifications.create(
+    'name-for-notification',{   
+    type: 'basic', 
+    iconUrl: 'ingress.png', 
+    title: "Guardian", 
+    message: "Mosaic couch under attack by cerebr0" 
+    },
+
+function() {} 
+
+);
