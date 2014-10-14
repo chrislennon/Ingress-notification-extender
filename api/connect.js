dd20104d32120b1fe8a297c1174333ee82b7ada0
Dion Segijn <dionsegijn@hotmail.com>
2014-10-14 18:58:14 +0200

    add GET function to connect.js

--- api/connect.js
+++ api/connect.js
@@ -14,3 +14,19 @@ function post(api, params) {
 	}
 	http.send(params);
 }
+
+function get(api, params) {
+	var http = new XMLHttpRequest();
+	var url = "dionsegijn.nl/notify/api/" + api + "/" + params;
+	http.open("GET", url, true);
+
+	//Send the proper header information along with the request
+	http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+	http.onreadystatechange = function() {//Call a function when the state changes.
+	    if(http.readyState == 4 && http.status == 200) {
+	        alert(http.responseText);
+	    }
+	}
+	http.send(params);
+}
