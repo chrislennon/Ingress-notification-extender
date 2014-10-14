de9da9ca12643809f242c6b3a3e0bc9c02f84f33
Dion Segijn <dionsegijn@hotmail.com>
2014-10-14 18:56:02 +0200

    add post function to connect.js

---
+++ api/connect.js
@@ -0,0 +1,16 @@
+function post(api, params) {
+	var http = new XMLHttpRequest();
+	var url = "dionsegijn.nl/notify/api/" + api;
+	var params = params;
+	http.open("POST", url, true);
+
+	//Send the proper header information along with the request
+	http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+	http.onreadystatechange = function() {//Call a function when the state changes.
+	    if(http.readyState == 4 && http.status == 200) {
+	        alert(http.responseText);
+	    }
+	}
+	http.send(params);
+}
